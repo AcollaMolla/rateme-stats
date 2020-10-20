8dd7de626bb4f361e77343a0d77a727874e41e00
Exekveras <antengman@gmail.com>
2020-10-20 20:41:38 +0200

    Create methods and return mock values

--- rateme-stats.py
+++ rateme-stats.py
@@ -1,10 +1,32 @@
 from user import User
 import requests
 
+def GetSex(post):
+	return "female"
+	
+def GetAge(post):
+	return 20
+	
+def GetRedditUserId(post):
+	return 123
+	
+def GetRedditPostId(post):
+	return 456
+	
+def CalculateStats(post):
+	stats = []
+	stats.append(5)
+	stats.append(1.2)
+	return stats
+	
+
 URL = "https://reddit.com/r/rateme/top/.json"
 users = []
 r = requests.get(URL, headers = {'User-agent': 'rateme-stats 1.0'})
 data = r.json()
 posts = data['data']['children']
+id = 0
 for post in posts:
-	print("post")
+	user = User(id, GetSex(post), GetAge(post), GetRedditUserId(post), GetRedditPostId(post), CalculateStats(post))
+	users.append(user)
+	id = id + 1
--- user.py
+++ user.py
@@ -1,9 +1,9 @@
 class User:
-	def __init__(self, id, sex, age, redditUserId, redditPostId, avgRate, stddevRate):
+	def __init__(self, id, sex, age, redditUserId, redditPostId, stats):
 		self.id = id
 		self.sex = sex
 		self.age = age
 		self.redditUserId = redditUserId
 		self.redditPostId = redditPostId
-		self.avgRate = avgRate
-		self.stddevRate = stddevRate 
+		self.avgRate = stats[0]
+		self.stddevRate = stats[1] 
