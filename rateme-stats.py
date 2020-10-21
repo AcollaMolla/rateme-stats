7d0b08b4907cf73e3e454ade55f6cac89d56f1e5
Exekveras <antengman@gmail.com>
2020-10-21 20:43:34 +0200

    Use Pusshift API instead. Extract male/female from title

--- rateme-stats.py
+++ rateme-stats.py
@@ -1,9 +1,9 @@
 from user import User
-import requests, re
+import requests, re, time
 
 def GetAgeSex(post):
 	pattern = "[a-zA-Z]\d{2}|\d{2}[a-zA-Z]"
-	title = post['data']['title']
+	title = post['title']
 	ageAndSex = re.search(pattern, title)
 	if ageAndSex is not None:
 		return ageAndSex.group(0)
@@ -14,9 +14,12 @@ def GetSex(post):
 	ageAndSex = GetAgeSex(post)
 	sex = re.search(pattern, ageAndSex).group(0)
 	print(sex)
+	print(post['title'])
 	if sex == "w" or sex == "W" or sex == "f" or sex == "F":
 		return "Female"
-	return "female"
+	elif sex == "m" or sex == "M":
+		return "Male"
+	return "None"
 	
 def GetAge(post):
 	return 20
@@ -33,12 +36,19 @@ def CalculateStats(post):
 	stats.append(1.2)
 	return stats
 	
+def CreateTimestamp():
+	#Fetch posts made at least 1 week ago, letting the post mature in votes and comments
+	oneWeekInSeconds = 604800
+	today = int(time.time())
+	print(today - oneWeekInSeconds)
+	return today - oneWeekInSeconds
+	
 
-URL = "https://reddit.com/r/rateme/top/.json?count=20"
+URL = "https://api.pushshift.io/reddit/search/submission/?subreddit=rateme&sort=desc&sort_type=created_utc&before=" + str(CreateTimestamp()) + "&size=5"
 users = []
 r = requests.get(URL, headers = {'User-agent': 'rateme-stats 1.0'})
 data = r.json()
-posts = data['data']['children']
+posts = data['data']
 id = 0
 print("posts length: " + str(len(posts)))
 for post in posts:
